a2005e0a657810d74ae4c0e85a0d74131ba3b84a
Ilyas Galiev <iljas-galiev@mail.ru>
2020-11-14 15:10:36 +0300

    dev

--- MedicineApplication/Startup.cs
+++ MedicineApplication/Startup.cs
@@ -31,10 +31,7 @@ namespace MedicineApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
